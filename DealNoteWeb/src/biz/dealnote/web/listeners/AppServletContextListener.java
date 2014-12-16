114d4ddf23687204b64c7fb020d7ca837f314b83
dimoonlda <dimoon.lda@gmail.com>
2014-12-16 21:33:49 +0200

    added ServletContextListener

---
+++ DealNoteWeb/src/biz/dealnote/web/listeners/AppServletContextListener.java
@@ -0,0 +1,18 @@
+package biz.dealnote.web.listeners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class AppServletContextListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+
+	}
+
+}
