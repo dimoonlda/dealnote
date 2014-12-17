2b4305bafa148cc501ba161015522c5f3906bdf8
dimoonlda <dimoon.lda@gmail.com>
2014-12-17 17:54:11 +0200

    added controller for GPS form

---
+++ DealNoteWeb/src/biz/dealnote/web/controllers/AgentGpsController.java
@@ -0,0 +1,59 @@
+package biz.dealnote.web.controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import biz.dealnote.web.beans.Agent;
+import biz.dealnote.web.beans.Location;
+import biz.dealnote.web.dao.DAOFactory;
+import biz.dealnote.web.dao.LocationDAO;
+
+public class AgentGpsController extends HttpServlet {
+	private DAOFactory base;
+	
+	private static final long serialVersionUID = -6625434898849602970L;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		base = (DAOFactory)config.getServletContext().getAttribute("base");
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		Integer agentID = null;
+		String agentDate = null;
+		try{
+			agentID = Integer.valueOf(req.getParameter("agentid"));
+			agentDate = req.getParameter("agentdate");
+		}catch(Exception e){
+			//TODO: Log
+		}
+		
+		List<Agent> agents = base.getAgentDAO().getActiveAgentsList();
+		req.setAttribute("agentsList", agents);
+		if (agentID != null && agentDate != null){
+			LocationDAO locationDAO = base.getLocationDAO();
+			List<Location> locList = locationDAO.getLocationList(agentID, agentDate);
+			req.setAttribute("locations", locList);			
+		}
+		//req.setAttribute("do", "showGPSLocations");
+		
+		RequestDispatcher view = req.getRequestDispatcher("/jsp/showAgentGps.jsp");
+		view.forward(req, resp);
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
