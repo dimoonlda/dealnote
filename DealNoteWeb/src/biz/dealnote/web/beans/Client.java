750993556b169d66208be042e9bed6bfef8aaf61
dimoonlda <dimoon.lda@gmail.com>
2014-12-20 23:37:02 +0200

    added Client bean class

---
+++ DealNoteWeb/src/biz/dealnote/web/beans/Client.java
@@ -0,0 +1,249 @@
+package biz.dealnote.web.beans;
+
+/**
+ * Client info
+ * @author Lutay D.A.
+ *
+ */
+public class Client {
+
+	private int id;
+	private String name;
+	private String phone;
+	private int clientTypeId;
+	private String taxCode;
+	private int agentId;
+	private String addressLocation;
+	private String taxNum;
+	private String okpo;
+	private String mfo;
+	private String bankName;
+	private String bankAccount;
+	private String dogNum;
+	private String fName;
+	private String addressLaw;
+	private int debtSumm1;
+	private int debtDays1;
+	private int debtSumm2;
+	private int debtDays2;
+	private short stopShipment;
+	private short routeId;
+	private int defaultDiscount;
+	private int parentId;
+	private double longitude;
+	private double latitude;
+	private int outerid;
+
+	public Client(){
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getClientTypeId() {
+		return clientTypeId;
+	}
+
+	public void setClientTypeId(int clientTypeId) {
+		this.clientTypeId = clientTypeId;
+	}
+
+	public String getTaxCode() {
+		return taxCode;
+	}
+
+	public void setTaxCode(String taxCode) {
+		this.taxCode = taxCode;
+	}
+
+	public int getAgentId() {
+		return agentId;
+	}
+
+	public void setAgentId(int agentId) {
+		this.agentId = agentId;
+	}
+
+	public String getAddressLocation() {
+		return addressLocation;
+	}
+
+	public void setAddressLocation(String addressLocation) {
+		this.addressLocation = addressLocation;
+	}
+
+	public String getTaxNum() {
+		return taxNum;
+	}
+
+	public void setTaxNum(String taxNum) {
+		this.taxNum = taxNum;
+	}
+
+	public String getOkpo() {
+		return okpo;
+	}
+
+	public void setOkpo(String okpo) {
+		this.okpo = okpo;
+	}
+
+	public String getMfo() {
+		return mfo;
+	}
+
+	public void setMfo(String mfo) {
+		this.mfo = mfo;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public String getBankAccount() {
+		return bankAccount;
+	}
+
+	public void setBankAccount(String bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+
+	public String getDogNum() {
+		return dogNum;
+	}
+
+	public void setDogNum(String dogNum) {
+		this.dogNum = dogNum;
+	}
+
+	public String getFName() {
+		return fName;
+	}
+
+	public void setFName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getAddressLaw() {
+		return addressLaw;
+	}
+
+	public void setAddressLaw(String addressLaw) {
+		this.addressLaw = addressLaw;
+	}
+
+	public int getDebtSumm1() {
+		return debtSumm1;
+	}
+
+	public void setDebtSumm1(int debtSumm1) {
+		this.debtSumm1 = debtSumm1;
+	}
+
+	public int getDebtDays1() {
+		return debtDays1;
+	}
+
+	public void setDebtDays1(int debtDays1) {
+		this.debtDays1 = debtDays1;
+	}
+
+	public int getDebtSumm2() {
+		return debtSumm2;
+	}
+
+	public void setDebtSumm2(int debtSumm2) {
+		this.debtSumm2 = debtSumm2;
+	}
+
+	public int getDebtDays2() {
+		return debtDays2;
+	}
+
+	public void setDebtDays2(int debtDays2) {
+		this.debtDays2 = debtDays2;
+	}
+
+	public short getStopShipment() {
+		return stopShipment;
+	}
+
+	public void setStopShipment(short stopShipment) {
+		this.stopShipment = stopShipment;
+	}
+
+	public short getRouteId() {
+		return routeId;
+	}
+
+	public void setRouteId(short routeId) {
+		this.routeId = routeId;
+	}
+
+	public int getDefaultDiscount() {
+		return defaultDiscount;
+	}
+
+	public void setDefaultDiscount(int defaultDiscount) {
+		this.defaultDiscount = defaultDiscount;
+	}
+
+	public int getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(int parentId) {
+		this.parentId = parentId;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public int getOuterId() {
+		return outerid;
+	}
+
+	public void setOuterId(int outerId) {
+		this.outerid = outerId;
+	}
+
+}
